72c5f039baf7505404f0e3c3290d7ecf87624140
Bryant Howell - ThoughtSpot <83678239+bryanthowell-ts@users.noreply.github.com>
2021-06-16 09:15:10 -0500

    Update tse.js

--- example_actions/linking_filter/tse.js
+++ example_actions/linking_filter/tse.js
@@ -17,20 +17,23 @@ const embed = () => {
 }
 
 const tsInit = () => {
+  
   init({
     thoughtSpotHost: tsURL,
-    authType: AuthType.None,
+    authType: AuthType.None, // In production, use appropriate AuthType for your SSO method
   });
 }
 
-let columnName = '';
-let values = [''];
+let columnNameToFilter = ''; // Fill in with the column name you want the values retrieved from and filtered on
+let filterValues = [''];  // Global array of filterValues, used in the runtimeFilters when the Pinboard is rendered. Starts blank so no filter is applied
 
+// Function to handle the payload response from the Custom Action 
+// Updates the global filterValues array, then re-runs the embedPinboard to reload the original Pinboard with the updated values in the runtimeFilters
 const filterData = (payload) => {
   const actionData = PinboardContextActionData.createFromJSON(payload);
   columnName = actionData.columnNames[0];
-  values = [];
-  values.push(actionData.data[columnName][0]);
+  filterValues = [];
+  filterValues.push(actionData.data[columnNametoFilter][0]);
   embedPinboard();
 }
 
@@ -39,16 +42,17 @@ const embedPinboard = () => {
   // Sample embed.  The pinboardId GUID needs to exist in your system.
   const embed = new PinboardEmbed("#embed", {
     frameParams: {},
-     pinboardId: "b22eabd5-6fa5-4342-847e-ca2abd5d54cc",
+     pinboardId: "b22eabd5-6fa5-4342-847e-ca2abd5d54cc", // Replace with the GUID of a Pinboard in your instance
      runtimeFilters: [{
-          columnName: columnName, // eg: color
+          columnName: columnNametoFilter, // eg: color
           operator: RuntimeFilterOp.EQ,
-          values: values // eg: red
+          values: filterValues // eg: red
      }],
   });
 
   embed
   .on(EmbedEvent.CustomAction, (payload) => {
+    // The id is defined when creating the Custom Action in ThoughtSpot. Checking id attribute allows correct routing of multiple Custom Actions
     if (payload.id === 'filter') {
       filterData(payload);
     }
